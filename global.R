eff7ae5d474af819d277c7ab2e15bf458ee75e4d
Klaudia <klaudiatunska@gmail.com>
2022-06-18 16:00:18 +0200

    Update global.R

--- global.R
+++ global.R
@@ -13,8 +13,8 @@ list.files("data",pattern = "csv",full.names = TRUE)
 
 file_paths =list.files("data",pattern = "csv",full.names = TRUE)
 
-food<-fread(file_paths)
-food<-food[Y2012> 0 & !is.na(Y2012) & !is.na(Y2010) & Y2010> 0 & !is.na(Y2011) 
+food = fread(file_paths)
+food = food[Y2012> 0 & !is.na(Y2012) & !is.na(Y2010) & Y2010> 0 & !is.na(Y2011) 
            & Y2011> 0 & !is.na(Y2013) & Y2013> 0 & !is.na(Y2014) & Y2014> 0 
            & !is.na(Y2015) & Y2015> 0 & !is.na(Y2016) & Y2016> 0 
            & !is.na(Y2017) & Y2017> 0 & !is.na(Y2018) & Y2018> 0 
@@ -28,6 +28,6 @@ food = food[, list(Area, Item, Element, Unit,
                    `2014` = Y2014, `2015` = Y2015, `2016` = Y2016,
                    `2017` = Y2017, `2018` = Y2018, `2019` = Y2019)]
 
-food <- food[Element %in% c("Production", "Import Quantity", "Export Quantity",
+food = food[Element %in% c("Production", "Import Quantity", "Export Quantity",
                             "Food")]
 
