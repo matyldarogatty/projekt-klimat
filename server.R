82a19f43359078314a4d0d1fc97ebd8772f525e2
vendia0 <315221@uwr.edu.pl>
2022-06-21 22:58:11 +0200

    Dodanie plotów - nie działają

--- server.R
+++ server.R
@@ -1,3 +1,17 @@
 server = function(input, output) {
-  barplot(veg_exp$`2010`)
+  output[["production_plot"]] = renderPlot({
+    item_year_plot(production, 'yearP', "productP")
+  })
+  
+  output[["consumption_plot"]] = renderPlot({
+    item_year_plot(consumption, 'yearC', "productC")
+  })
+  
+  output[["import_plot"]] = renderPlot({
+    item_year_plot(import, 'yearI', "productI")
+  })
+  
+  output[["export_plot"]] = renderPlot({
+    item_year_plot(export, 'yearE', "productE")
+  })
 }
