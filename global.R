1bf1f0ad8dd526429b552afe002434a618fc522c
vendia0 <315221@uwr.edu.pl>
2022-06-22 10:29:08 +0200

    dalsza praca nad wykresami

--- global.R
+++ global.R
@@ -15,10 +15,10 @@ library(shinyWidgets)
 food = fread('FoodData.csv')
 #food = fread(file_paths)
 food = food[Y2012> 0 & !is.na(Y2012) & !is.na(Y2010) & Y2010> 0 & !is.na(Y2011) 
-           & Y2011> 0 & !is.na(Y2013) & Y2013> 0 & !is.na(Y2014) & Y2014> 0 
-           & !is.na(Y2015) & Y2015> 0 & !is.na(Y2016) & Y2016> 0 
-           & !is.na(Y2017) & Y2017> 0 & !is.na(Y2018) & Y2018> 0 
-           & !is.na(Y2019) & Y2019> 0] 
+            & Y2011> 0 & !is.na(Y2013) & Y2013> 0 & !is.na(Y2014) & Y2014> 0 
+            & !is.na(Y2015) & Y2015> 0 & !is.na(Y2016) & Y2016> 0 
+            & !is.na(Y2017) & Y2017> 0 & !is.na(Y2018) & Y2018> 0 
+            & !is.na(Y2019) & Y2019> 0] 
 
 
 food = food[, list(Area, Item, Element, `2010` = Y2010,
@@ -27,7 +27,7 @@ food = food[, list(Area, Item, Element, `2010` = Y2010,
                    `2017` = Y2017, `2018` = Y2018, `2019` = Y2019)]
 
 food = food[Element %in% c("Production", "Import Quantity", "Export Quantity",
-                            "Food", "Tourist consumption")]
+                           "Food", "Tourist consumption")]
 # unit - all 1000 tonnes
 
 # wybrane produkty
@@ -38,8 +38,23 @@ products = c("Cereals - Excluding Beer", "Sugar & Sweeteners", "Pulses",
              'Eggs', "Fish, Seafood")
 food = food[Item %in% products]
 
+# ograniczenie krajów z małymi wartościami
 country = unique(food[["Area"]])[1:178]
 food = food[Area %in% country]
+
+food[, s := `2010` + `2011` + `2012` + `2013` + `2014` + `2015` + 
+       `2016` + `2017` + `2018` + `2019`]
+long = melt(food, id.vars = 'Area', measure.vars = 's')
+small = unique(long[, su := sum(value), 
+                    by = c('Area')][, list(Area, su)])[order(su)][, Area][1:60]
+food = food[!(Area %in% small)]
+food = food[!(Area %in% c('China, Hong Kong SAR', 'China, mainland', 
+                          'China, Taiwan Province of', "C\xf4te d'Ivoire"))]
+
+
+# wywalać chiny inne niż chiny
+
+# podział na Elementy
 
 import =  food[Element =="Import Quantity"]
 import[, Element := NULL]
@@ -67,14 +82,15 @@ choose_product = function(dt, str_product){
 veg_exp = choose_product(export, "Vegetables")
 
 ### WYKRESY
-item_year_plot = function(dt, year, item){
+item_year_plot = function(DT, year, item){
+  dt = choose_year(DT, year)
   g = ggplot(dt[Item == item], aes(x = Area, y = get(year), fill = Area)) + 
-  geom_bar(stat = 'identity') +
-  labs(title = 'Amount of ___ in countries', 
-       y = "Amount of product (1000 tonnes)", x = "Country") +
-  theme_minimal() +
-  coord_flip() +
-  theme(legend.position = "None") 
+    geom_bar(stat = 'identity') +
+    labs(title = 'Amount of ___ in countries', 
+         y = "Amount (1000 tonnes)", x = "Country") +
+    theme_minimal() +
+    coord_flip() +
+    theme(legend.position = "None", axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
   return(g)
 }
 item_year_plot(import, '2018', 'Pulses')
@@ -83,7 +99,7 @@ area_year_plot = function(dt, year, area){
   g = ggplot(dt[Area == area], aes(x = Item, y = get(year), fill = Item)) + 
     geom_bar(stat = 'identity') +
     labs(title = 'Amount of products in ___', 
-         y = "Amount of product (1000 tonnes)", x = "Product") +
+         y = "Amount (1000 tonnes)", x = "Product") +
     theme_minimal() +
     theme(legend.position = "None") +
     coord_flip()
