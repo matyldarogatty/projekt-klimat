8b618639cdb25e52b7fffa243e694d8fa44590a5
vendia0 <315221@uwr.edu.pl>
2022-06-22 19:40:07 +0200

    robocze wykresy

--- scratch.R
+++ scratch.R
@@ -1,55 +1,117 @@
+library(ggplot2)
 library(data.table)
-library(lubridate)
-library(rbenchmark)
-list.files()
+library(stringi)
 
-file_paths = list.files("data_meat", pattern = "csv", full.names=TRUE)
-food_files = vector("list", length(file_paths))
-
-for (i in seq_along(file_paths)){
-  food_files[[i]]=fread(file_paths[i])
-}
-
-head(food_files[[1]])
-
-food_list = lapply(file_paths, fread)
-food_list[[1]]
-fad= food_files[[1]] #food_all_data
-food_flags = food_files[[2]]
-food_balance_sheets = food_files[[3]]
-
-class(fad)
-nrow(fad)
-ncol(fad)
-head(fad)
-summary(fad)
-str(fad)
-dplyr::glimpse(fad)
-fad[4]
-colnames(fad)
+food = fread('FoodData.csv')
+#food = fread(file_paths)
+food = food[Y2012> 0 & !is.na(Y2012) & !is.na(Y2010) & Y2010> 0 & !is.na(Y2011) 
+            & Y2011> 0 & !is.na(Y2013) & Y2013> 0 & !is.na(Y2014) & Y2014> 0 
+            & !is.na(Y2015) & Y2015> 0 & !is.na(Y2016) & Y2016> 0 
+            & !is.na(Y2017) & Y2017> 0 & !is.na(Y2018) & Y2018> 0 
+            & !is.na(Y2019) & Y2019> 0] 
 
 
-fad = fad[, colnames(fad) %in% c("Area", "Item", "Element", "Y2010","Y2011","Y2012","Y2013",
-                                 "Y2014","Y2015","Y2016","Y2017","Y2018","Y2019", "Unit"), with =FALSE]
-fad = fad[!is.na(Y2010) & !is.na(Y2011) & !is.na(Y2012) & !is.na(Y2013) & !is.na(Y2014) & !is.na(Y2015) &
-            !is.na(Y2016) & !is.na(Y2017) & !is.na(Y2018) & !is.na(Y2019)]
+food = food[, list(Area, Item, Element, `2010` = Y2010,
+                   `2011` = Y2011, `2012` = Y2012,`2013` = Y2013,
+                   `2014` = Y2014, `2015` = Y2015, `2016` = Y2016,
+                   `2017` = Y2017, `2018` = Y2018, `2019` = Y2019)]
 
-import_export = fad[Element =="Import Quantity" | Element == "Export Quantity"]
-import =  fad[Element =="Import Quantity"]
-export =  fad[Element =="Export Quantity"]
+food = food[Element %in% c("Production", "Import Quantity", "Export Quantity",
+                           "Food", "Tourist consumption")]
+# unit - all 1000 tonnes
+
+# wybrane produkty
+products = c("Cereals - Excluding Beer", "Sugar & Sweeteners", "Pulses", 
+             "Nuts and products", "Vegetables", "Fruits - Excluding Wine", 
+             "Wine", "Beer", 'Spices', "Bovine Meat",
+             'Mutton & Goat Meat', "Poultry Meat", "Milk - Excluding Butter", 
+             'Eggs', "Fish, Seafood", "Pigmeat")
+food = food[Item %in% products]
+
+# ograniczenie krajów z małymi wartościami
+food = food[Area %in% unique(food[["Area"]])[1:178]]
+
+food[, s := `2010` + `2011` + `2012` + `2013` + `2014` + `2015` + 
+       `2016` + `2017` + `2018` + `2019`]
+long = melt(food, id.vars = 'Area', measure.vars = 's')
+small = unique(long[, su := sum(value), 
+                    by = c('Area')][, list(Area, su)])[order(su)][, Area][1:60]
+food = food[!(Area %in% small)]
+food = food[!(Area %in% c('China, Hong Kong SAR', 'China, mainland', 
+                          'China, Taiwan Province of', "C\xf4te d'Ivoire"))]
+
+country = unique(food[, Area])
+food[, s := NULL]
+# wywalać chiny inne niż chiny
+
+# podział na Elementy
+
+import =  food[Element =="Import Quantity"]
+import[, Element := NULL]
+
+export =  food[Element =="Export Quantity"]
+export[, Element := NULL]
+
+production = food[Element =="Production"]
+production[, Element := NULL]
+
+consumption = food[Element == 'Food' | Element == 'Tourist consumption']
+consumption[, Element := NULL]
 
 ### FUNKCJE
 choose_year = function(data_table, year_str){
-  return (data_table[, colnames(data_table) %in% c("Area", "Item", "Element", year_str, "Unit"), 
+  return (data_table[, colnames(data_table) %in% 
+                       c("Area", "Item", "Element", year_str), 
                      with =FALSE])
 }
-imp_2018 = choose_year(import, "Y2018")
+imp_2018 = choose_year(import, "2018")
 
 choose_product = function(dt, str_product){
   return(dt[Item == str_product])
 }
 veg_exp = choose_product(export, "Vegetables")
 
-choose_row = function(col_name, str_value) fad_not_na[col_name == str_value]
+choose_area = function(dt, str_area){
+  return(dt[Area == str_area])
+}
+china_exp = choose_area(export, "China")
 
-barplot(veg_exp[[5]], names=veg_exp[[1]])
+### WYKRESY
+item_year_plot = function(DT, year, item){
+  dt = choose_product(choose_year(DT, year), item)
+  g = ggplot(reorder(dt), aes(x = Area, y = get(year), fill = Area)) + 
+    geom_bar(stat = 'identity') +
+    labs(title = paste(c('Amount of'), item, c('in countries in'), year), 
+         y = "Amount (1000 tonnes)", x = "Country") +
+    theme_minimal() +
+    coord_flip() +
+    theme(legend.position = "None") 
+  return(g)
+}
+item_year_plot(import, '2018', 'Pulses')
+
+area_year_plot = function(DT, year, area){
+  dt = choose_area(choose_year(DT, year), area)
+  g = ggplot(dt, aes(x = Item, y = get(year), fill = Item)) + 
+    geom_bar(stat = 'identity') +
+    labs(title = paste(c('Amount of products in'), area, c('in'), year), 
+         y = "Amount (1000 tonnes)", x = "Product") +
+    theme_minimal() +
+    theme(legend.position = "None") +
+    coord_flip()
+  return(g)
+}
+area_year_plot(import, '2018', 'China')
+
+area_line = function(DT, area){
+  dt = choose_area(DT, area)
+  g = ggplot(melt(dt), aes(x = variable, y = value, group = Item, color = Item)) + 
+    geom_line() +
+    geom_point() +
+    labs(title = paste(c('Amount of products in'), area, c('over the years')), 
+       y = "Amount (1000 tonnes)", x = "Year") +
+    theme_minimal() +
+    theme(legend.title = element_blank(), legend.position = "bottom")
+  return(g)
+}
+area_line(import, 'China')
