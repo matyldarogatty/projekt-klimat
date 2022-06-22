904af98048c081ed9ce9facebb5ffe739dd76ee3
Klaudia <klaudiatunska@gmail.com>
2022-06-22 19:10:11 +0200

    zmiany

--- server.R
+++ server.R
@@ -1,1 +1,23 @@
+<<<<<<< Updated upstream
 
+=======
+server = function(input, output) {
+  output[["production_plot"]] = renderPlot({
+    item_year_plot(production, input[['yearP']], input[["productP"]])
+  })
+  output[["summary"]] = renderDT({
+    food[Element=="Production"]
+  })
+  output[["consumption_plot"]] = renderPlot({
+    item_year_plot(consumption, input[['yearC']], input[["productC"]])
+  })
+  
+  output[["import_plot"]] = renderPlot({
+    item_year_plot(import, input[['yearI']], input[["productI"]])
+  })
+  
+  output[["export_plot"]] = renderPlot({
+    item_year_plot(export, input[['yearE']], input[["productE"]])
+  })
+}
+>>>>>>> Stashed changes
--- ui.R
+++ ui.R
@@ -1,7 +1,121 @@
+<<<<<<< Updated upstream
 ui = fluidPage(
   tabsetPanel(
     tabPanel("Produkcja"),
     tabPanel("Spożycie"),
     tabPanel("Import i eksport")
   ))
+=======
+ui = fluidPage(theme = shinytheme("united"),
+    titlePanel("World food analysis"),
+        mainPanel(
+          tabsetPanel(
+        tabPanel("Production",
+              tabsetPanel(
+                tabPanel("Plot",  
+                 p("Wykresy dot. produkcji - najlepiej barploty, mapka"),
+                 sidebarPanel(
+                   
+                   selectInput(inputId = "productP", 
+                               label = "Choose product",
+                               choices = products,
+                               selected = 1),
+                   
+                   selectInput(inputId = "regionP", 
+                               label = "Choose region",
+                               choices = country,
+                               selected = 1),
+                   
+                   sliderInput(inputId = 'yearP',
+                               label = 'Choose year',
+                               min = 2010,
+                               max = 2019,
+                               value = c(2010),
+                               sep = ''),
+                   plotOutput(outputId = "production_plot"))),
+                 tabPanel("Table",
+                          dataTableOutput("summary"))
+                   
+                 )),
+        
+        tabPanel("Consumption",
+                 p("Wykresy dot konsumpcji"),
+                 sidebarPanel(
+                   selectInput(inputId = "productC", 
+                               label = "Choose product",
+                               choices = products,
+                               selected = 1),
+                   
+                   selectInput(inputId = "regionC", 
+                               label = "Choose region",
+                               choices = country,
+                               selected = 1),
+                   
+                   sliderInput(inputId = 'yearC',
+                               label = 'Choose year',
+                               min = 2010,
+                               max = 2019,
+                               value = c(2010),
+                               sep = ''),
+                   plotOutput(outputId = "consumption_plot")
+                   )),
+        
+        tabPanel("Import",
+                 p('Wykresy dot importu'),
+                 sidebarPanel(
+                   selectInput(inputId = "productI", 
+                               label = "Choose product",
+                               choices = products,
+                               selected = 1),
+                   
+                   selectInput(inputId = "regionI", 
+                               label = "Choose region",
+                               choices = country,
+                               selected = 1),
+                   
+                   sliderInput(inputId = 'yearI',
+                               label = 'Choose year',
+                               min = 2010,
+                               max = 2019,
+                               value = c(2010),
+                               sep = ''),
+                   plotOutput(outputId = "import_plot")
+                 )),
+        
+        tabPanel("Export",
+                 p('Wykresy dot exportu'),
+                 sidebarPanel(
+                   selectInput(inputId = "productE", 
+                               label = "Choose product",
+                               choices = products,
+                               selected = 1),
+                   
+                   selectInput(inputId = "regionE", 
+                               label = "Choose region",
+                               choices = country,
+                               selected = 1),
+                   
+                   sliderInput(inputId = 'yearE',
+                               label = 'Choose year',
+                               min = 2010,
+                               max = 2019,
+                               value = c(2010),
+                               sep = ''),
+                   plotOutput(outputId = "export_plot")
+                 )),
+        
+        tabPanel("Information",
+                 column(width = 6,
+                        h3("Authors"),
+                        p("Matylda Rogatty"),
+                        p("Aleksandra Ziętek"),
+                        p("Klaudia Tuńska")),
+                 column(width = 6,
+                        h3("Data"),
+                        p("All of the data in the application comes 
+                          from www.fao.org, a website of Food and Agriculture 
+                          Organization of the United Nations."))))
+))
+        
+>>>>>>> Stashed changes
 
